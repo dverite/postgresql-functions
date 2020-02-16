469ff216cbe30e1123e2bfe4301ea83353cecb8d
Daniel Vérité <dverite@gmail.com>
2020-02-16 20:45:56 +0100

    Prioritize the longest match rather than the shortest.

--- strings/plperl/multi_replace_plperl.sql
+++ strings/plperl/multi_replace_plperl.sql
@@ -1,28 +1,24 @@
 /*
-  Substitute strings by other strings within a larger string, with
-  Perl s// operator, in a single pass.
-  Each element in @orig is replaced by the element at the same index
-  in @repl
-  If multiple strings in the array match simultaneously, the smallest one
-  wins, or, in the case of equal lengths, the string before in
-  alphabetic order.
+  Substitute substrings within a larger string, with Perl s// operator,
+  in a single pass. Each element in @orig found in @string (scanned left
+  to right) is replaced by the element at the same index in @repl.
+  When multiple strings in the array match simultaneously, the longest one
+  wins.
 */
 CREATE OR REPLACE FUNCTION multi_replace(string text, orig text[], repl text[])
 RETURNS text
 AS $BODY$
   my ($string, $orig, $repl) = @_;
   my %subs;
-  my $i=0;
 
   # Check that the arrays are of the same size, unidimensional,
-  # and don't contain null values.
+  # and contain no null values.
   if (@$orig != @$repl) {
      elog(ERROR, "array sizes mismatch");
   }
   if (ref @$orig[0] eq 'ARRAY' || ref @$repl[0] eq 'ARRAY') {
-     elog(ERROR, "array dimensions mismatch");
+     elog(ERROR, "multi-dimensional arrays are not allowed");
   }
-
   if (grep { !defined } (@$orig, @$repl)) {
      elog(ERROR, "null elements are not allowed");
   }
@@ -32,13 +28,12 @@ AS $BODY$
   @subs{@$orig} = @$repl;
 
   # Build a regexp of the form (s1|s2|...)
-  # with the substrings sorted to have deterministic results
-  # in the cases of multiple matches.
+  # with the substrings sorted to match longest first
   my $re = join "|", map quotemeta,
-     sort { (length($a) <=> length($b)) || $a cmp $b } keys %subs;
+     sort { (length($b) <=> length($a)) } keys %subs;
   $re = qr/($re)/;
 
-  # The order will be used in the matching because (from perlre):
+  # The order will be kept in matching because (from perlre):
   # "Alternatives are tried from left to right, so the first alternative
   # found for which the entire expression matches, is the one that is
   # chosen"
